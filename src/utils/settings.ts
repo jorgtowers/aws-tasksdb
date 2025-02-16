66fd1b491dfecf92de50599d36b51686e54a118a
Jorge L. Torres A. <jorgtowers@gmail.com>
2025-02-16 18:10:25 -0400

    add relative path to constants

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,3 +1,3 @@
-export const K_API_PATH='http://localhost:3000/api';
-export const K_APP_PATH='http://localhost:3000';
-export const K_PAGES_TASKS_PATH='http://localhost:3000/tasks';
+export const K_API_PATH='/api';
+export const K_APP_PATH='/';
+export const K_PAGES_TASKS_PATH='/tasks';
