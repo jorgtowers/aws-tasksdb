15b644382539cfec75692ba3f17c61221e1e076b
Jorge L. Torres A. <jorgtowers@gmail.com>
2025-02-16 19:15:12 -0400

    settings for aws

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,3 +1,3 @@
-export const K_API_PATH='/api';
-export const K_APP_PATH='/';
-export const K_PAGES_TASKS_PATH='/tasks';
+export const K_API_PATH='https://main.d73h9kl9l360s.amplifyapp.com/api';
+export const K_APP_PATH='https://main.d73h9kl9l360s.amplifyapp.com/';
+export const K_PAGES_TASKS_PATH='https://main.d73h9kl9l360s.amplifyapp.com/tasks';
