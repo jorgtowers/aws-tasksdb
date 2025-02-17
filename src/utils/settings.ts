ca84c1b1df18dbc293818fe3ba152cac49985f1d
Jorge L. Torres A. <jorgtowers@gmail.com>
2025-02-16 23:56:05 -0400

    update new site

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,5 +1,5 @@
 //const K_HOST_NAME='http://localhost:3000';
-const K_HOST_NAME='https://main.d73h9kl9l360s.amplifyapp.com';
+const K_HOST_NAME='https://main.d2q6cztsmerc69.amplifyapp.com';
 export const K_API_PATH=`${K_HOST_NAME}/api`;
 export const K_APP_PATH=`${K_HOST_NAME}`;
 export const K_PAGES_TASKS_PATH=`${K_HOST_NAME}/tasks`;
